e422e1f35f8e36f812d5c81a510e525029258f54
lisavincent31 <lisa.vincent31150@gmail.com>
2024-07-15 10:46:01 +0200

    add templates twig to access to the app

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
